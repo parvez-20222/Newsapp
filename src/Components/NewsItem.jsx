ba3c2d38a56351f61ec8fbb4c2160705fc6cb0c6
Parvez Alam <parvezalam74520@gmail.com>
2023-01-07 11:04:17 +0530

    nnn

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -8,11 +8,11 @@ export default class NewsItem extends Component {
                 <div className="card">
                     <img src={this.props.pic} height="200px" className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{this.props.title.slice(0,50)+"..."}</h5>
+                        <h5 className="card-title">{this.props.title.slice(0,40)+"..."}</h5>
                         <h6 className='card-source'>{this.props.source} - {`${new Date(this.props.date).getDate()}/${new Date(this.props.date).getMonth()}/${new Date(this.props.date).getFullYear()}`} </h6>
                         <hr />
-                        <p className="card-text">{this.props.description.slice(0,150)+"..."}</p>
-                        <a href={this.props.url} className="btn background text-light w-100 btn-sm">Read Full Article</a>
+                        <p className="card-text">{this.props.description.slice(0,130)+"..."}</p>
+                        <a target="blank" href={this.props.url} className="btn background text-primary w-100 btn-sm">Read Full Article</a>
                     </div>
                 </div>
             </div>
